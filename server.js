655c48ad4695b9dd394fb6ebf13a21ea37533568
kevin13469 <150281175+kevin13469@users.noreply.github.com>
2024-05-27 02:13:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const FilePath = 'db.json'
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://kevin13469.github.io');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
